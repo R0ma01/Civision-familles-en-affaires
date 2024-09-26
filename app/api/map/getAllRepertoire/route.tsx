51bdf8c4558c4a56ecc96381fb98f2852ccc1030
R0ma01 <romaine.brand@polymtl.ca>
2024-09-26 10:50:42 -0400

    some fixes

--- app/api/map/getAllRepertoire/route.tsx
+++ app/api/map/getAllRepertoire/route.tsx
@@ -24,8 +24,8 @@ export async function GET(req: Request) {
                 { status: 400 },
             );
         }
-        const result = await collection
-            .find(
+        const result = [
+            await collection.findOne(
                 {
                     // Apply dynamic filters here using the spread operator
                     ...filtersObj, // This will inject the filters object into the query
@@ -37,8 +37,9 @@ export async function GET(req: Request) {
                         NOM_ASSUJ: 1,
                     },
                 },
-            )
-            .toArray();
+            ),
+        ];
+        // .toArray();
 
         if (!result) {
             return NextResponse.json(
