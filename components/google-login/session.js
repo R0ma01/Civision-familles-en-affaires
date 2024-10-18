73b4d980eae04a6d2c1819a5fe9616d9c2a9888f
R0ma01 <romaine.brand@polymtl.ca>
2024-10-18 13:17:21 -0400

    ajout des login

--- components/google-login/session.js
+++ components/google-login/session.js
@@ -1,26 +1,39 @@
 'use server';
 import { auth } from '@/auth';
 import { closeDatabase, connectToDatabase } from '@/utils/auth-mongoDB-utils';
+import { generateAdminToken, generateToken } from '@/utils/token-utils';
 
+const { ADMIN_ID } = process.env;
 export default async function something() {
     try {
         const session = await auth();
-        console.log('Session:', session);
+
         if (session) {
-            console.log('hello');
             const collection = await connectToDatabase();
             const email = session.user.email;
-            console.log(email);
+
             const user = await collection.findOne({
                 email,
             });
-            console.log(session.user);
-            console.log(user);
-            await closeDatabase();
+
             if (!user) {
                 return;
             } else {
-                return user;
+                const lastLoginDate = new Date();
+                await collection.updateOne(
+                    { email },
+                    { $set: { lastLoginDate } },
+                );
+                const token = generateToken({ userId: user._id }, '24h');
+                let adminToken = null;
+
+                if (user._id == ADMIN_ID) {
+                    adminToken = generateAdminToken(
+                        { userId: user._id },
+                        '24h',
+                    );
+                }
+                return { token, adminToken };
             }
         }
     } catch (e) {
