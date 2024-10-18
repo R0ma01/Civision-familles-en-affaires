73d7ddca000cdd45c64e686385739211a72a6ded
R0ma01 <romaine.brand@polymtl.ca>
2024-10-18 14:00:18 -0400

    ajout des login

--- components/component/sidebar/sidebar.tsx
+++ components/component/sidebar/sidebar.tsx
@@ -45,7 +45,7 @@ const Sidebar: React.FC = () => {
 
     useEffect(() => {
         async function check() {
-            console.log(localStorage.getItem('token'));
+            console.log('SideBar: ' + localStorage.getItem('token'));
             const newUser = await checkToken();
 
             setUser(newUser);
